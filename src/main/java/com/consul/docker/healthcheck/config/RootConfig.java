8609e4c7ae329c535089335d5fb6f7caa87e8c73
zhoulin2 <zhoulin2@asiainfo.com>
2019-09-19 15:55:35 +0800

    添加 consul token 支持

--- src/main/java/com/consul/docker/healthcheck/config/RootConfig.java
+++ src/main/java/com/consul/docker/healthcheck/config/RootConfig.java
@@ -3,23 +3,54 @@ package com.consul.docker.healthcheck.config;
 import com.google.common.net.HostAndPort;
 import com.orbitz.consul.Consul;
 import com.orbitz.consul.KeyValueClient;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Steven
  * @date 2019-05-16
  */
 @Configuration
+@Slf4j
 public class RootConfig {
 
 	@Value("${consul.address}")
 	private String address;
 
+	@Value("${consul.token}")
+	private String token;
+
 	@Bean
 	public Consul consul() {
-		Consul consul = Consul.builder().withHostAndPort(HostAndPort.fromString(address)).build();
+
+		log.info("consul cluster address: " + address);
+		log.info("consul token: " + token);
+
+		String[] hostAndPort = StringUtils.split(address, ',');
+		List<HostAndPort> hostAndPortList = new ArrayList<>();
+		for (String hap : hostAndPort) {
+			hostAndPortList.add(HostAndPort.fromString(hap));
+		}
+
+		Consul consul = null;
+		Consul.Builder builder = Consul.builder();
+
+		if (StringUtils.isNoneBlank(token)) {
+			builder.withAclToken(token);
+		}
+
+		if (hostAndPortList.size() == 1) {
+			consul = builder.withHostAndPort(HostAndPort.fromString(address)).build();
+		} else {
+			consul = builder.withMultipleHostAndPort(hostAndPortList, 5000).build();
+		}
+
 		return consul;
 	}
 
